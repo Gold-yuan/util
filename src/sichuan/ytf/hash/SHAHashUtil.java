fda3d9080f86c2ee7ca7ce06182fde345f37f011
adminytf <adminytf@ADMINIB-MTROAK7.crl.ibm.com>
2018-01-26 17:44:40 +0800

    加密算法

---
+++ src/sichuan/ytf/hash/SHAHashUtil.java
@@ -0,0 +1,84 @@
+package sichuan.ytf.hash;
+
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
+/**
+ * 该算法计算消息摘要 顺序有关
+ */
+public class SHAHashUtil {
+
+	/**
+	 * @param strSrc
+	 *            要加密的字符串
+	 * @return SHA-256 加密算法的字符串
+	 */
+	public static String encryptSHA256(String strSrc) {
+		if (strSrc == null || strSrc.trim().length() == 0) {
+			return null;
+		}
+		return encrypt(strSrc, "SHA-256");
+	}
+
+	public static String encryptSHA256(byte[] bt) {
+		if (bt == null)
+			return null;
+		return encryptSHA256(bt, "SHA-256");
+	}
+
+	public static String encrypt(byte[] bt, String encName) {
+		if (bt == null)
+			return null;
+		return encryptSHA256(bt, encName);
+	}
+
+	/**
+	 * 对字符串加密,加密算法使用MD5, SHA-1,SHA-224,SHA-256,SHA-384,SHA-512。
+	 * 
+	 * 主要适用于数字签名标准（DigitalSignature Standard DSS）里面定义的数字签名算法（Digital Signature
+	 * Algorithm DSA）。
+	 * 
+	 * @param strSrc
+	 *            要加密的字符串
+	 * @param encName
+	 *            加密类型
+	 * @return
+	 */
+	public static String encrypt(String strSrc, String encName) {
+		return encryptSHA256(strSrc.getBytes(), encName);
+	}
+
+	private static String encryptSHA256(byte[] bt, String name) {
+		MessageDigest md = null;
+		String strDes = null;
+		try {
+			md = MessageDigest.getInstance(name);
+			md.update(bt);
+			strDes = bytes2Hex(md.digest()); // to HexString
+		} catch (NoSuchAlgorithmException e) {
+			return null;
+		}
+		return strDes;
+	}
+
+	private static String bytes2Hex(byte[] bts) {
+		StringBuffer sb = new StringBuffer();
+		for (int i = 0; i < bts.length; i++) {
+			String hex = Integer.toHexString(bts[i] & 0xFF);
+			if (hex.length() == 1) {
+				hex = '0' + hex;
+			}
+			sb.append(hex.toUpperCase());
+		}
+		return sb.toString();
+	}
+
+	public static void main(String args[]) {
+		String[] en = { "MD5", "SHA-1", "SHA-224", "SHA-256", "SHA-384", "SHA-512" };
+
+		for (int i = 0; i < en.length; i++) {
+			String s2 = encrypt("my name is typhoon", en[i]);
+			System.out.println(s2 + " \n" + s2.length() + "位  " + en[i]);
+		}
+	}
+}
