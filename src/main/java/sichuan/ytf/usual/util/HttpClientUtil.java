68094ab133ed0875163cf06677fe21ca41f99192
bjyuantf <bjyuantf@cn.ibm.com>
2018-05-15 17:19:33 +0800

    添加restful风格请求

--- src/main/java/sichuan/ytf/usual/util/HttpClientUtil.java
+++ src/main/java/sichuan/ytf/usual/util/HttpClientUtil.java
@@ -15,6 +15,7 @@ import org.apache.http.client.methods.CloseableHttpResponse;
 import org.apache.http.client.methods.HttpDelete;
 import org.apache.http.client.methods.HttpGet;
 import org.apache.http.client.methods.HttpPost;
+import org.apache.http.client.methods.HttpPut;
 import org.apache.http.client.methods.HttpRequestBase;
 import org.apache.http.entity.ContentType;
 import org.apache.http.entity.StringEntity;
@@ -27,10 +28,15 @@ import org.apache.http.message.BasicNameValuePair;
 import org.apache.http.util.EntityUtils;
 
 /**
+ * 发送http、https请求工具类
+ * 
+ * 可发送get post delete put类请求， 参数形式包括url、map、json、file
+ * 
  * @author adminytf
  *
  */
 public class HttpClientUtil {
+	/** 超时时间 */
 	private RequestConfig requestConfig = RequestConfig.custom().setSocketTimeout(300000).setConnectTimeout(300000)
 			.setConnectionRequestTimeout(300000).build();
 
@@ -47,7 +53,54 @@ public class HttpClientUtil {
 	}
 
 	/**
-	 * 发送 post请求
+	 * 发送http、https请求 包括 get post delete put
+	 * 
+	 * @param {@link
+	 * 			HttpRequestBase}
+	 * @return
+	 * @throws IOException
+	 *             连接失败
+	 */
+	private String sendHttpReq(HttpRequestBase req) throws IOException {
+		CloseableHttpClient httpClient = null;
+		CloseableHttpResponse response = null;
+		HttpEntity entity = null;
+		String responseContent = "";
+
+		// 创建默认的httpClient实例.
+		httpClient = HttpClients.createDefault();
+		req.setConfig(requestConfig);
+		// 执行请求
+		try {
+			// 创建默认的httpClient实例.
+			httpClient = HttpClients.createDefault();
+			req.setConfig(requestConfig);
+			// 执行请求
+			response = httpClient.execute(req);
+			entity = response.getEntity();
+			if (entity != null) {
+				responseContent = EntityUtils.toString(entity, "UTF-8");
+			}
+		} catch (IOException e) {
+			throw e;
+		} finally {
+			try {
+				// 关闭连接,释放资源
+				if (response != null) {
+					response.close();
+				}
+				if (httpClient != null) {
+					httpClient.close();
+				}
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
+		return responseContent;
+	}
+
+	/**
+	 * 发送 post 无参数请求
 	 * 
 	 * @param httpUrl
 	 *            地址
@@ -58,7 +111,7 @@ public class HttpClientUtil {
 	}
 
 	/**
-	 * 发送 post请求
+	 * 发送 post 类get请求
 	 * 
 	 * @param httpUrl
 	 *            地址
@@ -100,7 +153,7 @@ public class HttpClientUtil {
 	}
 
 	/**
-	 * 发送 post请求
+	 * 发送 post 带参数map请求
 	 * 
 	 * @param httpUrl
 	 *            地址
@@ -123,7 +176,7 @@ public class HttpClientUtil {
 	}
 
 	/**
-	 * 发送 post请求（带文件）
+	 * 发送 post 带文件请求
 	 * 
 	 * @param httpUrl
 	 *            地址
@@ -169,50 +222,18 @@ public class HttpClientUtil {
 		return sendHttpReq(httpPost);
 	}
 
-	/**
-	 * 发送http请求
-	 * 
-	 * @param httpPost
-	 * @return
-	 * @throws IOException
-	 *             连接失败
-	 */
-	private String sendHttpReq(HttpRequestBase req) throws IOException {
-		CloseableHttpClient httpClient = null;
-		CloseableHttpResponse response = null;
-		HttpEntity entity = null;
-		String responseContent = "";
-
-		// 创建默认的httpClient实例.
-		httpClient = HttpClients.createDefault();
-		req.setConfig(requestConfig);
-		// 执行请求
-		try {
-			// 创建默认的httpClient实例.
-			httpClient = HttpClients.createDefault();
-			req.setConfig(requestConfig);
-			// 执行请求
-			response = httpClient.execute(req);
-			entity = response.getEntity();
-			if (entity != null) {
-				responseContent = EntityUtils.toString(entity, "UTF-8");
-			}
-		} catch (IOException e) {
-			throw e;
-		} finally {
-			try {
-				// 关闭连接,释放资源
-				if (response != null) {
-					response.close();
-				}
-				if (httpClient != null) {
-					httpClient.close();
-				}
-			} catch (IOException e) {
-				e.printStackTrace();
-			}
-		}
-		return responseContent;
+	protected String buildSoapRequestData() {
+		StringBuffer soapRequestData = new StringBuffer();
+		soapRequestData.append(
+				"<soapenv:Envelope xmlns:soapenv=\"http://schemas.xmlsoap.org/soap/envelope/\" xmlns:web=\"webserviceNameSpace\">");// 命名空间如：http://xxx.xxx/xxx
+		soapRequestData.append("<soapenv:Header/>");
+		soapRequestData.append("<soapenv:Body>");
+		soapRequestData.append("<web:methodName>");// 方法名
+		soapRequestData.append("<arg0>这里是参数</arg0>"); // 参数，可多个 arg0 arg1 arg2...
+		soapRequestData.append(" </web:methodName>");
+		soapRequestData.append("</soapenv:Body>");
+		soapRequestData.append("</soapenv:Envelope>");
+		return soapRequestData.toString();
 	}
 
 	/**
@@ -234,5 +255,41 @@ public class HttpClientUtil {
 	public String sendHttpDelete(String httpUrl) throws IOException {
 		HttpDelete httpDelete = new HttpDelete(httpUrl);
 		return sendHttpReq(httpDelete);
+	}
+
+	/**
+	 * 发送 Http PUT请求
+	 * 
+	 * @param httpUrl
+	 * @return
+	 * @throws IOException
+	 */
+	public String sendHttpPut(String httpUrl) throws IOException {
+		HttpPut httpPut = new HttpPut(httpUrl);
+		return sendHttpReq(httpPut);
+	}
+
+	public String sendHttpPut(String httpUrl, Map<String, String> maps) throws IOException {
+		HttpPut httpPut = new HttpPut(httpUrl);
+		// 创建参数队列
+		List<NameValuePair> nameValuePairs = new ArrayList<NameValuePair>();
+		for (String key : maps.keySet()) {
+			nameValuePairs.add(new BasicNameValuePair(key, maps.get(key)));
+		}
+		try {
+			httpPut.setEntity(new UrlEncodedFormEntity(nameValuePairs, "UTF-8"));
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return sendHttpReq(httpPut);
+	}
+
+	public String sendHttpJsonPut(String httpUrl, String json) throws IOException {
+		HttpPut httpPut = new HttpPut(httpUrl);
+		// 设置参数
+		StringEntity stringEntity = new StringEntity(json, "UTF-8");
+		stringEntity.setContentType("application/json");
+		httpPut.setEntity(stringEntity);
+		return sendHttpReq(httpPut);
 	}
 }
