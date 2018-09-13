68f88bebac37b0d7671222af6d43044fc16dd65e
yuan <bjyuantf@cn.ibm.com>
2018-09-13 10:22:58 +0800

    连接mysql8

---
+++ src/main/java/sichuan/ytf/jdbc/ConnMysql8.java
@@ -0,0 +1,23 @@
+package sichuan.ytf.jdbc;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class ConnMysql8 {
+	public static void main(String[] args) throws ClassNotFoundException, SQLException {
+		Class.forName("com.mysql.cj.jdbc.Driver");
+		Connection conn = DriverManager.getConnection(
+				"jdbc:mysql://9.186.54.96:3306/springboot_test?characterEncoding=utf8", "root", "MySqlPass1!");
+		Statement st = conn.createStatement();
+		ResultSet rs = st.executeQuery("select*from u_user");
+		while (rs.next()) {
+			for (int i = 1; i <= 4; i++) {
+				System.out.print(rs.getString(i) + "\t");
+			}
+			System.out.println();
+		}
+	}
+}
